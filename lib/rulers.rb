035d7b5b8c734bc0cf0d79845341ae38212e6615
frozenfung <frozenfung@gmail.com>
2017-12-30 20:14:27 +0800

    Init routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,24 @@
 require "rulers/array"
 require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Rulers 😎 "]]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
 
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
