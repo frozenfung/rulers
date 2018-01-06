2160f57380b061dded07bbf7ec7f6eb8c637d47f
frozenfung <frozenfung@gmail.com>
2018-01-06 20:59:31 +0800

    Exercise 3

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -5,6 +5,10 @@ require "rulers/routing"
 module Rulers
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico' || env['PATH_INFO'] == 'robots.txt'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -2,7 +2,13 @@ module Rulers
   class Application
     def get_controller_and_action(env)
       _, cont, action, after = env["PATH_INFO"].split("/", 4)
-      cont = cont.capitalize + "Controller"
+
+      STDERR.puts cont
+      if cont.empty?
+        return [Object.const_get("QuotesController"), "base_quote"]
+      else
+        cont = cont.capitalize + "Controller"
+      end
 
       [Object.const_get(cont), action]
     end
