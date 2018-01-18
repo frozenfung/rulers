ed5dc2774e76d9ae76eda53579451313533e7baf
frozenfung <frozenfung@gmail.com>
2018-01-18 23:42:21 +0800

    Finish exercise 3

--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -3,7 +3,6 @@ module Rulers
     def get_controller_and_action(env)
       _, cont, action, after = env["PATH_INFO"].split("/", 4)
 
-      STDERR.puts cont
       if cont.empty?
         return [Object.const_get("QuotesController"), "base_quote"]
       else
